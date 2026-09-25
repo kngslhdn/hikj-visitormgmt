035c92e25acceb456f7ab6ac1249b16024f75e6d
kngslhdn <kangsolehudin@gmail.com>
2026-09-25 18:55:09 +0700

    fix: centralize SECUREOPS publishable key

--- assets/secureops-config.js
+++ assets/secureops-config.js
@@ -1,4 +1,5 @@
 window.SECUREOPS_CONFIG=Object.freeze({
   SUPABASE_URL:'https://hmqgmusellcetakoalva.supabase.co',
-  SUPABASE_FUNCTIONS_URL:'https://hmqgmusellcetakoalva.supabase.co/functions/v1'
+  SUPABASE_FUNCTIONS_URL:'https://hmqgmusellcetakoalva.supabase.co/functions/v1',
+  SUPABASE_PUBLISHABLE_KEY:'sb_publishable_J6P-baCAZOoVxj2rHBZMkA_Ux4U5_mQ'
 });
