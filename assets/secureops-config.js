30166aa96c0a05e11ecb181a6df192e4f92095bc
kngslhdn <kangsolehudin@gmail.com>
2026-09-25 18:53:36 +0700

    feat: centralize SECUREOPS backend configuration

---
+++ assets/secureops-config.js
@@ -0,0 +1,4 @@
+window.SECUREOPS_CONFIG=Object.freeze({
+  SUPABASE_URL:'https://hmqgmusellcetakoalva.supabase.co',
+  SUPABASE_FUNCTIONS_URL:'https://hmqgmusellcetakoalva.supabase.co/functions/v1'
+});
